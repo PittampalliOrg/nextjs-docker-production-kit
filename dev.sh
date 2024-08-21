1262bc5207256fce0893146552a54da581fcb141
Vinod Pittampalli <vinod@pittampalli.com>
2024-08-21 20:09:41 +0000

    supabase and next.js app are both working

--- dev.sh
+++ dev.sh
@@ -1,16 +1,20 @@
-# Stop all running containers
-docker kill $(docker ps -aq) && docker rm $(docker ps -aq)
+#!/bin/bash
 
-# Create a network, which allows containers to communicate
-# with each other, by using their container name as a hostname
-docker network create my_network
+# Set the Docker Compose file to use
+COMPOSE_FILE="docker-compose.dev.yml"
 
-# Build dev
-# Note: Keep v1 command until "Use Docker Compose v2" is enabled by default for Docker Desktop for Linux
-# Docker aliases `docker-compose` (v1 command) to `docker compose` (v2 command), but not the other way around
-docker-compose -f docker-compose.dev.yml build
+# Stop and remove only the containers defined in the compose file
+echo "Stopping and removing containers defined in $COMPOSE_FILE..."
+docker compose -f $COMPOSE_FILE down
 
-# Up dev
-# --renew-anon-volumes
-#   postgres/mysql retrieve volumes from previous containers after being killed
-docker-compose -f docker-compose.dev.yml up --renew-anon-volumes
+# Remove images for services defined in the compose file
+echo "Removing images for services defined in $COMPOSE_FILE..."
+docker compose -f $COMPOSE_FILE down --rmi all
+
+# Build and start the containers
+echo "Building and starting containers..."
+docker compose -f $COMPOSE_FILE up --build
+
+# Show the logs
+echo "Showing logs..."
+docker compose -f $COMPOSE_FILE logs -f
