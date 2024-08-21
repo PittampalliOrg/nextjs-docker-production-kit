c67d7e7a519148782cf97785e1856af0417f4554
Vinod Pittampalli <vinod@pittampalli.com>
2024-08-21 21:22:28 +0000

    supabase and next.js app are working

--- .devcontainer/dev.sh
+++ .devcontainer/dev.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-# Set the Docker Compose file to use
-COMPOSE_FILE="docker-compose.dev.yml"
+# Set the Docker Compose file path
+COMPOSE_FILE="/home/vpittamp/nextjs-docker-production-kit/.devcontainer/docker-compose.dev.yml"
 
 # Stop and remove only the containers defined in the compose file
 echo "Stopping and removing containers defined in $COMPOSE_FILE..."
 docker compose -f $COMPOSE_FILE down
-
-# Remove images for services defined in the compose file
-echo "Removing images for services defined in $COMPOSE_FILE..."
-docker compose -f $COMPOSE_FILE down --rmi all
 
 # Build and start the containers
 echo "Building and starting containers..."
